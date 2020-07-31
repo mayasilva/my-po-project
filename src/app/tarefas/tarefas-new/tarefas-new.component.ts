a94cc9969cee652b01b179e5f70f6731d69900d9
Mayara Alves Da Silva <mama_silva@hotmail.com>
2020-07-30 21:49:44 -0300

    cria formulario para incluir tarefas

--- src/app/tarefas/tarefas-new/tarefas-new.component.ts
+++ src/app/tarefas/tarefas-new/tarefas-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoDynamicFormField } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-tarefas-new',
@@ -7,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarefasNewComponent implements OnInit {
 
+  fields: Array<PoDynamicFormField> = [
+    {
+      property: 'Descricao',
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+      gridColumns: 6,
+      gridSmColumns: 12,
+      order: 1
+    },
+    {
+      property: 'data',
+      label: 'Data',
+      type: 'date',
+      format: 'mm/dd/yyyy',
+      gridColumns: 6,
+      gridSmColumns: 12,
+      order: -1
+    },
+    {
+      property: 'categoria',
+      gridColumns: 6,
+      options: [
+        { label: 'Casa', value: 1 },
+        { label: 'Trabalho', value: 2 },
+        { label: 'Estudos', value: 3 },
+        { label: 'Estudos', value: 3 },
+      ]
+    },
+    {
+      property: 'concluido',
+      label: 'Concluido',
+      type: 'boolean',
+    },]  
   constructor() { }
+
 
   ngOnInit(): void {
   }
